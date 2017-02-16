d9161a244a71f8f7958990e9165e43d6de8690de
Roger Butenuth <roger.butenuth@codecentric.de>
2017-02-16 14:55:26 +0100

    Rewrite to fetch everything automatically

--- lib/logstash/inputs/cloudhub_api.rb
+++ lib/logstash/inputs/cloudhub_api.rb
@@ -1,49 +1,100 @@
-# logstash-input-cloudhub
-plugin for logstash, which pulls logs from a application deployed to [Cloudhub](anypoint.mulesoft.com/cloudhub).
+# encoding: utf-8
 
-# try it out
-`Dockerfile` and `docker-compose.yml` contains everything you need to try it out, assumed you already have docker-compose installed and a Docker instance running.
+require "net/http"
+require "json"
 
-## build the plugin from source.
-```
-# gem can be used instead of jgem
-jgem build logstash-input-cloudhub.gemspec
-```
+class CloudhubAPI
+  def initialize logger, organization_id, username, password, events_per_call, proxy_host=nil, proxy_port=nil, proxy_username=nil, proxy_password=nil
+    @logger = logger
+    @organization_id = organization_id
+    @username = username
+    @password = password
+    @events_per_call = events_per_call
+    @proxy_host=proxy_host
+    @proxy_port=proxy_port
+    @proxy_username=proxy_username
+    @proxy_password=proxy_password
+  end
 
-## setup a container with logstash 2.4 and install the plugin
-```
-docker-compose build
-```
+  def token
+    uri = URI.parse('https://anypoint.mulesoft.com/accounts/login')
 
-## run the container with logstash and the plugin.
-```
-export CLOUDHUB_DOMAIN=<application>
-export CLOUDHUB_USERNAME=<username>
-export CLOUDHUB_PASSWORD=<password> 
-docker-compose up
-```
+    client = Net::HTTP.new(uri.host, uri.port, @proxy_host, @proxy_port, @proxy_username, @proxy_password)
+    client.use_ssl = true
 
-# configuration
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.body = URI.encode_www_form({ 
+      "username" => @username, 
+      "password" => @password 
+    })
 
-```
-input { 
-    cloudhub { 
-        domain => "my-application" 
-        username => "Bob" 
-        password => "secret"
-        interval => 300
-        startTime => 0
-        proxy_host => "squid"
-        proxy_port => 3128
-        proxy_username => "user"
-        proxy_password => "secret"
-    } 
-} 
+    response = client.request(request)
+    return JSON.parse(response.body)['access_token']
+  end
 
-output { 
-    stdout {} 
-}
-```
+  # Returns an array of hashes, sample hash:
+  # {"id"=>"...", "name"=>"prod", "organizationId"=>"...", "isProduction"=>true}
+  def environments cached_token=token
+    uri = URI.parse("https://anypoint.mulesoft.com/accounts/api/organizations/#{@organization_id}")
+    
+    client = Net::HTTP.new(uri.host, uri.port, @proxy_host, @proxy_port, @proxy_username, @proxy_password)
+    client.use_ssl = true
+    
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.add_field("Authorization", "Bearer #{cached_token}")
+    response = client.request(request)
+    
+    body = JSON.parse(response.body)
+    
+    return body['environments']
+  end
+  
+  # Returns an array of hashes, for us is interesting:
+  # { "domain"=>"my_name", "fullDomain"=>"my_name.eu.cloudhub.io", ... }
+  def apps environment, cached_token=token
+    uri = URI.parse("https://anypoint.mulesoft.com/cloudhub/api/v2/applications")
+    client = Net::HTTP.new(uri.host, uri.port, @proxy_host, @proxy_port, @proxy_username, @proxy_password)
+    client.use_ssl = true
 
-# further information
-[Cloudhub Enhanced Logging API 1.0.0](https://anypoint.mulesoft.com/apiplatform/anypoint-platform/#/portals/organizations/68ef9520-24e9-4cf2-b2f5-620025690913/apis/34348/versions/35742/pages/49591)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.add_field("Authorization", "Bearer #{cached_token}")
+    request.add_field("X-ANYPNT-ENV-ID", environment['id'])
+    
+    response = client.request(request)
+    
+    return JSON.parse(response.body)
+  end
+  
+  def logs startTime, environment_id, application, cached_token=token
+    uri = URI.parse("https://anypoint.mulesoft.com/cloudhub/api/v2/applications/#{application}/logs")
+
+    client = Net::HTTP.new(uri.host, uri.port, @proxy_host, @proxy_port, @proxy_username, @proxy_password)
+    client.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.add_field("Authorization", "Bearer #{cached_token}")
+    request.content_type = 'application/json'
+    request.body = JSON.generate({
+      :startTime => startTime,
+      :endTime => java.lang.Long::MAX_VALUE,
+      :limit => @events_per_call, 
+      :descending => false
+    })
+    request.add_field("X-ANYPNT-ENV-ID", environment_id)
+    retries = 10
+    while retries > 0
+      response = client.request(request)
+      begin
+        parsed_logs = JSON.parse(response.body)
+        return parsed_logs
+      rescue
+        retries -= 1
+        if (retries == 0)
+          @logger.error("Can't parse logs: " + response.body)
+        end
+        Stud.stoppable_sleep(5)
+      end
+    end
+    return []
+  end
+end
